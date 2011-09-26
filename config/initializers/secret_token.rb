82ae8b6d6ed96c557d4b978285edce97824aaa7b
Hsu Han Ooi <hsuhan@decide.com>
2011-09-25 19:06:48 -0700

    add project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seadu::Application.config.secret_token = '940d46da542e0a54ed441cf17924fc53eb5cccc84d498d5a74ecd99b218e984c209a459d5c0d836ec21342597bf2f2b46d338152d909b93db2660b78161515d2'
