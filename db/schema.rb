a0bbbd13d69dff607ff9c87b6fea5fecde0df42a
Austinc <cargol@Blue-Monk-Monastery.local>
2011-10-01 03:46:12 -0700

    Updated initial migration

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@
 ActiveRecord::Schema.define(:version => 20111001060119) do
 
   create_table "questions", :force => true do |t|
-    t.integer  "status"
+    t.string   "status"
     t.integer  "room_id"
     t.string   "content"
     t.datetime "created_at"
@@ -23,7 +23,8 @@ ActiveRecord::Schema.define(:version => 20111001060119) do
 
   create_table "rooms", :force => true do |t|
     t.string   "name"
-    t.integer  "mood_threshold"
+    t.string   "status"
+    t.integer  "vibe_threshold"
     t.integer  "question_threshold"
     t.integer  "teacher_id"
     t.datetime "created_at"
@@ -42,7 +43,7 @@ ActiveRecord::Schema.define(:version => 20111001060119) do
   end
 
   create_table "votes", :force => true do |t|
-    t.integer  "vote_type"
+    t.string   "vote_type"
     t.integer  "question_id"
     t.datetime "created_at"
     t.datetime "updated_at"
