80c9788e8167a62cee3b43f6d19b50081a0a3a29
Austinc <cargol@d-69-91-175-75.dhcp4.washington.edu>
2011-10-01 15:56:59 -0700

    Fixed question vote count bug

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,13 +1,7 @@
 class StudentsController < ApplicationController
   def view
     @room = Room.find params[:room_id]
-    @questions = Question.select(
-      "questions.*, COUNT(votes.id) as votes_count"
-    ).joins(
-      "JOIN votes on votes.question_id = questions.id"
-    ).group(
-      "votes.question_id"
-    ).where("questions.room_id=#{@room.id}")
+    @questions = room.questions.highest_rated
     
     if mobile?
       render :template => "students/mobile"
