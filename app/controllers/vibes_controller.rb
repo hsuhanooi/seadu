058314b776a6087a52071ac2ea31bd9434fd5f33
fuJiin <fu7iin@gmail.com>
2011-10-01 16:33:54 -0700

    fix room_id in vibes_controller

--- app/controllers/vibes_controller.rb
+++ app/controllers/vibes_controller.rb
@@ -1,11 +1,10 @@
 class VibesController < ApplicationController
   def create
-    room_id   = params[:id]
+    room_id   = params[:room_id]
     vibe_type = params[:vibe_type]
     @vibe = Vibe.new(:room_id => room_id, :vibe_type => vibe_type)
     
-    if @vibe.valid?
-      @vibe.save
+    if @vibe.save!
       respond_to do |format|
         format.html { redirect_to students_view_url(room_id) }
         format.js { render :text => "Added vibe successfully" }
