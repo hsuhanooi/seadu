e631be57b24c52b428e6e2049991c4641cd9e957
fuJiin <fu7iin@gmail.com>
2011-10-01 11:19:22 -0700

    integerate compass, colors stylesheet

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Seadu
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += %W(#{config.root}/lib)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
@@ -44,5 +44,9 @@ module Seadu
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Compass load paths
+    config.sass.load_paths << "#{Gem.loaded_specs["compass"].full_gem_path}/frameworks/compass/stylesheets"
+    config.sass.load_paths << "#{Gem.loaded_specs["compass"].full_gem_path}/frameworks/blueprint/stylesheets"
   end
 end
