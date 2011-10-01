e41f2508ded625a18921aaa973b888f1675d9b52
fuJiin <fu7iin@gmail.com>
2011-10-01 15:04:57 -0700

    hook in vibes links, move student logic to students controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,16 @@
 class StudentsController < ApplicationController
   def view
-    @room = Room.find(params[:room_id])
-    @question = Question.new
+    @room = Room.find params[:room_id]
+    @questions = Question.select(
+      "questions.*, COUNT(votes.id) as votes_count"
+    ).joins(
+      "JOIN votes on votes.question_id = questions.id"
+    ).group(
+      "votes.question_id"
+    ).where("questions.room_id=#{@room.id}")
+    
+    if mobile?
+      render :template => "students/mobile"
+    end
   end
 end
