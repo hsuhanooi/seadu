6bb836308a97ab69aeb0b381d5dda36f354747db
Hsu Han Ooi <hsuhan@decide.com>
2011-10-01 00:46:11 -0700

    commit rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -13,18 +13,14 @@ class RoomsController < ApplicationController
     @room = Room.new(params[:room])
     
     if @room.valid?
-      render :action => :new
+      @room.save
+      redirect_to :action => :show, :id => @room.id
     else
-      redirect_to :action => :show
+      render :action => :new
     end
   end
   
   def show
     @room = Room.find params[:id]
-  end
-  
-  def admin
-    @room = Room.find params[:id]
-  end
-  
+  end  
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -2,5 +2,7 @@ class Room < ActiveRecord::Base
   belongs_to :teacher
   has_many :vibe
   
+  validates_uniqueness_of :name
+  
   validates_length_of :name, :within => 1..255
 end
