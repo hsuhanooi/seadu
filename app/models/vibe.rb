77b239c54870998290c40e0a7780119120a46cd7
Hsu Han Ooi <hsuhan@decide.com>
2011-10-02 11:50:56 -0700

    check in animations

--- app/models/vibe.rb
+++ app/models/vibe.rb
@@ -4,6 +4,7 @@ class Vibe < ActiveRecord::Base
   belongs_to :room
   
   PollInterval = 5
+  Decay = 120
   VIBE_TYPES = ['bored', 'good', 'confused']
   
   scope :bored, where(vibe_type: 'bored')
@@ -23,7 +24,7 @@ class Vibe < ActiveRecord::Base
   end
   
   def self.chart_live(room, last_ping)
-    last_format = (last_ping - PollInterval).utc.strftime("%Y-%m-%d %H:%M:%S")
+    last_format = (last_ping - PollInterval - Decay).utc.strftime("%Y-%m-%d %H:%M:%S")
     sql = "SELECT count(1) as room_id, vibe_type FROM `vibes` where room_id = #{room.id} and created_at > '#{last_format}' GROUP BY vibe_type"
     seconds = (Time.now - room.created_at).round
     hash = {:bored => [seconds, 0], :confused => [seconds, 0]}
@@ -43,36 +44,35 @@ class Vibe < ActiveRecord::Base
     str = ""
     chart_data = Vibe.chart(room_id)
     room = Room.find(room_id)
-    if !chart_data.empty?
-      started_at = room.created_at
-      ended_at = room.ended_at
-      last_time = (ended_at - started_at).round
-      ChartSeriesOrder.each{|v|
-        vibes = chart_data.select{|s| s.vibe_type == v}.sort{|x, y| x.created_at <=> y.created_at}
-        str << "{\n"
-        str << "name: '#{v}',\n"
-        data = ["{ x: 0, y: null}", "{ x: #{last_time}, y: 0}"]
-        idx = 0
-        last_count = 0
-        (0..last_time).each{|time|
-          if time % 60 == 0
-            point = vibes[idx]
-            if point
-              v_created = (point.created_at - room.created_at).round
-              if v_created <= time
-                data << "{ x: #{time}, y: #{point.room_id}}"
-                last_count = point.room_id
-                idx += 1
-              else
-                data << "{ x: #{time}, y: #{last_count}}"
-              end
+    started_at = room.created_at
+    ended_at = room.ended_at
+    last_time = (ended_at - started_at).round
+    max_time = last_time > 7200 ? 7200 : last_time
+    ChartSeriesOrder.each{|v|
+      vibes = chart_data.select{|s| s.vibe_type == v}.sort{|x, y| x.created_at <=> y.created_at}
+      str << "{\n"
+      str << "name: '#{v}',\n"
+      data = ["{ x: 0, y: null}", "{ x: #{max_time}, y: '0'}"]
+      idx = 0
+      last_count = 0
+      (0..last_time).each{|time|
+        if time % 60 == 0
+          point = vibes[idx]
+          if point
+            v_created = (point.created_at - room.created_at).round
+            if v_created <= time
+              data << "{ x: #{time}, y: #{point.room_id}}"
+              last_count = point.room_id
+              idx += 1
+            else
+              data << "{ x: #{time}, y: #{last_count}}"
             end
           end
-        }
-        str << "data: [#{data.join(', ')}]\n"
-        str << "},\n"
+        end
       }
-    end
+      str << "data: [#{data.join(', ')}]\n"
+      str << "},\n"
+    }
     return str.chop
   end
 end
