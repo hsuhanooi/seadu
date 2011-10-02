d852760a04e3453396a1dec28db588906f2cc976
Hsu Han Ooi <hsuhan@decide.com>
2011-10-02 13:42:46 -0700

    add engaged

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -43,6 +43,15 @@ class RoomsController < ApplicationController
     render :text => hash.to_json
   end
   
+  def finished_rooms
+    @rooms = []
+    created_at = Time.now - 3600 - rand(3600)
+    ended_at = created_at + 1800 + rand(3600)
+    ["Bma-Pitch-SpeakerBuddi-101", "Bma-Pitch-SpeakerBuddi-102", "Bma-Pitch-SpeakerBuddi-103", "Bma-Pitch-SpeakerBuddi-104", "Bma-Pitch-SpeakerBuddi-105", "Bma-Pitch-SpeakerBuddi-106"].each{|name|
+      @rooms << Room.new(:name => "", :status => 'expired', :vibe_threshold => 15, :question_threshold => 15, :teacher_id => 1, :num_listeners => rand(100) + 50, :created_at => created_at, :updated_at => created_at, :ended_at => ended_at)
+    }
+  end
+  
   def finish_room
     room = Room.find(params[:room_id])
     saved = room.finish_room
@@ -50,8 +59,9 @@ class RoomsController < ApplicationController
     respond_to do |format|
       format.html { 
         if saved
-          flash[:success] = "Room has been closed successfully."
-          redirect_to teachers_view_url(room.id)
+          # flash[:success] = "Room has been closed successfully."
+          # redirect_to teachers_view_url(room.id)
+          redirect_to finished_rooms_url(room.id)
         else
           flash[:error] = "There was a problem closing your room. Please try again."
           redirect_to teachers_view_url(room.id)
